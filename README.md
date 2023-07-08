3ae9fc513bb2f90a3f0263ae8228345f0b79d958
mhj <5261387+markhj@users.noreply.github.com>
2023-07-08 09:59:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+This is a "starter pack" for getting into OpenGL with C++. It utilizes the vcpkg package manager which is required to install dependencies.
+
+# Prerequisites
+
+* vcpkg (and basic knowledge of how it works). It is used in **manifest mode**.
+* C++ environment, for example CLion or Visual Studio
+
+# Libraries
+
+There are plenty of library choices out there for OpenGL. In this particular application we use:
+
+- GLFW
+- GLEW
+- GLM
+- Assimp
+
+Replacing them with alternatives shouldn't be too complicated, if you wish to go with other libraries.
+
+# Getting started
+
+Clone the project and open it in your favorite IDE.
+
+From console/command line run:
+
+````bash
+vcpkg install
+````
+
+The build is tested on Windows 64-bit with MinGW/GCC compiler.
+You may need to tweak vcpkg to get it to work with other compilers.
+
+It should now be possible to compile the project and see a window with a colored triangle.
---
+++ src/main.cpp
@@ -0,0 +1,139 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void errorCallback(int error, const char* description) {
+    std::cerr << "GLFW Error " << error << ": " << description << std::endl;
+}
+
+// Vertex shader source code
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPosition;
+    layout (location = 1) in vec3 aColor;
+
+    out vec3 fragmentColor;
+
+    void main() {
+        gl_Position = vec4(aPosition, 1.0);
+        fragmentColor = aColor;
+    }
+)";
+
+// Fragment shader source code
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    in vec3 fragmentColor;
+    out vec4 fragColor;
+
+    void main() {
+        fragColor = vec4(fragmentColor, 1.0);
+    }
+)";
+
+int main() {
+    // Initialize GLFW
+    glfwSetErrorCallback(errorCallback);
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Create a GLFW window
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Example", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the OpenGL context current
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        std::cerr << "GLEW Error: " << glewGetErrorString(err) << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Check OpenGL version
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Create the shader program and link the shaders
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Define the triangle vertices and colors
+    GLfloat vertices[] = {
+            -0.5f, -0.5f, 0.0f,  // Vertex 1 (position)
+            1.0f, 0.0f, 0.0f,    // Vertex 1 (color)
+            0.5f, -0.5f, 0.0f,   // Vertex 2 (position)
+            0.0f, 1.0f, 0.0f,    // Vertex 2 (color)
+            0.0f, 0.5f, 0.0f,    // Vertex 3 (position)
+            0.0f, 0.0f, 1.0f     // Vertex 3 (color)
+    };
+
+    // Create and bind the vertex array object (VAO)
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Create the vertex buffer object (VBO) and load vertex data
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Set the vertex attribute pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Clear the screen
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Use the shader program
+        glUseProgram(shaderProgram);
+
+        // Draw the triangle
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Clean up
+    glDeleteProgram(shaderProgram);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
+
+    glfwTerminate();
+
+    return 0;
+}
